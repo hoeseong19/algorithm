431ad25fce9044149999010638dd88f81d032899
hoeseong <hoeseong19@naver.com>
2022-12-11 02:48:43 +0900

    #queue

--- programmers/스택큐/기능개발/main.py
+++ programmers/스택큐/기능개발/main.py
@@ -1,55 +1,30 @@
-class Feature():
-    def __init__(self, progress, speed):
-        self.progress = progress
-        self.speed = speed
-
-    def process(self):
-        self.progress += self.speed
-
-    def is_finished(self):
-        is_finished = self.progress >= 100
-
-        return is_finished
+from collections import deque
 
 
 def solution(progresses, speeds):
     answer = []
 
-    feature_length = len(progresses)
-    feature_list = []
-    for i in range(feature_length):
-        progress = progresses[i]
-        speed = speeds[i]
+    q_progress = deque(progresses)
+    q_speed = deque(speeds)
 
-        feature = Feature(progress, speed)
-        feature_list.append(feature)
+    count = 0
 
-    # 작업은 순차적임
-    # 끝나야 되는 작업의 순서를 인덱스로 저장
-    finished_feature_count = 0
+    while q_progress:
+        if q_progress[0] >= 100:
+            q_progress.popleft()
+            q_speed.popleft()
 
-    while (finished_feature_count < feature_length):
-        for feature in feature_list[finished_feature_count:]:
-            if not feature.is_finished():
-                feature.process()
-
-        # 끝나는 날은 중요하지 않음
-        # 작업이 끝나는 시점에 얼마나 많은 작업이 끝났는 지가 중요
-        count = 0
-        for feature in feature_list[finished_feature_count:]:
-            if not feature.is_finished():
-                break
-            # 만약 하나라도 끝났으면
-            # 완료된 다음 작업까지, 혹은 마지막까지 순회
-
-            finished_feature_count += 1
             count += 1
-
-        # 작업이 완료되지 않은 경우, 여태까지 완료된 작업의 수를 저장
-        if count != 0:
+        else:
             answer.append(count)
+            count = 0
+            for index_progress in range(len(q_progress)):
+                q_progress[index_progress] += q_speed[index_progress]
 
-    return answer
+    answer.append(count)
 
-print(solution([93, 30, 55],[1, 30, 5])==[2, 1])
-print(solution([95, 90, 99, 99, 80, 99],[1, 1, 1, 1, 1, 1])==[1, 3, 2])
+    return [i for i in answer if i != 0]
+
+
+print(solution([93, 30, 55], [1, 30, 5]) == [2, 1])
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]) == [1, 3, 2])
