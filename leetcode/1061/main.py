94af02fb4331ed1cd14f2e129f64e113066b4b55
hoeseong <hoeseong19@naver.com>
2023-01-16 22:19:17 +0900

    #1061 #String #Union Find

--- leetcode/1061/main.py
+++ leetcode/1061/main.py
@@ -1,60 +1,52 @@
-from collections import defaultdict, deque
-from heapq import heappop, heappush
+class DisjointSet:
+    def __init__(self):
+        self.root = {}
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        # lexicographically smallest characters are heads
+        if rootX <= rootY:
+            self.root[rootY] = rootX
+        else:
+            self.root[rootX] = rootY
 
 
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
         answer = ''
 
-        graph = defaultdict(set)
+        disjoint_set = DisjointSet()
 
-        dict_char_TO_lexicographically_smallest_equivalent_char = {}
+        for x, y in zip(s1, s2):
+            if x not in disjoint_set.root:
+                disjoint_set.root[x] = x
+            if y not in disjoint_set.root:
+                disjoint_set.root[y] = y
+            disjoint_set.union(x, y)
 
-        length_string = len(s1)
+        list_base_str = list(baseStr)
+        for i, c in enumerate(list_base_str):
+            if c in disjoint_set.root:
+                list_base_str[i] = disjoint_set.find(c)
 
-        for i in range(length_string):
-            graph[s1[i]].add(s2[i])
-            graph[s2[i]].add(s1[i])
-
-        def bfs(char):
-            list_equivalent_char = []
-            q_char = deque([char])
-
-            while q_char:
-                c = q_char.popleft()
-
-                heappush(list_equivalent_char, c)
-
-                for equivalent_char in graph[c]:
-                    if equivalent_char not in list_equivalent_char:
-                        q_char.append(equivalent_char)
-
-            lexicographically_smallest_equivalent_char = heappop(
-                list_equivalent_char)
-
-            for equivalent_char in list_equivalent_char:
-                dict_char_TO_lexicographically_smallest_equivalent_char[
-                    equivalent_char] = lexicographically_smallest_equivalent_char
-
-        set_string = set(s1).union(set(s2))
-
-        for c in set_string:
-            if c not in dict_char_TO_lexicographically_smallest_equivalent_char:
-                bfs(c)
-
-        for k, v in dict_char_TO_lexicographically_smallest_equivalent_char.items():
-            baseStr = baseStr.replace(k, v)
-
-        answer = baseStr
+        answer = ''.join(list_base_str)
 
         return answer
 
 
-# print(Solution().smallestEquivalentString(
-#     s1="parker", s2="morris", baseStr="parser") == "makkek")
-# print(Solution().smallestEquivalentString(
-#     s1="hello", s2="world", baseStr="hold") == "hdld")
-# print(Solution().smallestEquivalentString(s1="leetcode",
-#       s2="programs", baseStr="sourcecode") == "aauaaaaada")
-
-print(Solution().smallestEquivalentString(s1 = "cjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnh", s2 = "gkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdej", baseStr = "rcomcrewoankfivfgfcfkgeadtnmhagjczisqblyldidsuhfdsooviilaaosgswjbqzhkwfwdgqbnuypegvsqoborpnhtjjaxcpjwtxowutansksxzzydoottmknysmpxyetwhlghwbwqvsasmwjsdcgzgfudhettrasxayqdtjkykeyxvqzuvwtlfnoyvauiilqtfstoeilmzbncnxildnjaaoxezuflkthcgdjneycktavzmsovdtxpxjtcfxfkyvsfxnzrwdnnjcnkplctexlgqzipfvqcxfawzdznhbhtdkhfoirliqdfehsiviiimntnuzyfhnbgxakhozldclhxjzpgzyabxwrrfalqdfeaxtvkkucslhrypsjexjdhbeuuckolfgrvxecuhslcbezuxsoklmqpmobybqausuneushwgrrtafujsljzpprklapncxlwktpitejlujycahvvfvggrzayolskjvaauocfikptwiyntvqejfdxirsbmqqrilnmcopskbtdlozevmbiaxraspymfnyaxgazfxsuoygeqzoccmsxamtpcquxvrpsqzdqxaaouyjnrmqxlsvfwsfnmeqyvvufkvlzhzpsfqojrdntpxekahrknbrjtzmhrasbynxwnzfoishiaqmomkcgfsokrdmxmjyxrmrsaphcxicbfubzbvaeuevvvsidhgtzhxevtjxwiwriqkomltbaenbgxxdvzdcuddixkudbcoplqvyyzrkkkjxlqilowootcapiygayfpqzlhdfemmweynumqocfjlicesvbziglxvuqrimxbntrpaebvcbvdeqkepgnejlfkynpqzfamfylvogkakhgeuqqiegsvjoxjfgoiadlevwnwdwyljvvdzrzgiryhmepannhyfzvzxxbaxnqfrgnpksxomdojqvquwqagomfjubsrkxmnwdwfixbwmegdljzcjtdonufsoweesjlvjqivx"))
+print(Solution().smallestEquivalentString(
+    s1="parker", s2="morris", baseStr="parser") == "makkek")
+print(Solution().smallestEquivalentString(
+    s1="hello", s2="world", baseStr="hold") == "hdld")
+print(Solution().smallestEquivalentString(s1="leetcode",
+      s2="programs", baseStr="sourcecode") == "aauaaaaada")
+print(Solution().smallestEquivalentString(s1="cjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnhcjgbnhojkkmgbmgddajmicdafgklkkdogaebeomcikdbafleihmjmbnmanokfbdkibhabdmdgbencabfikhnccjeceiolggbahnh",
+      s2="gkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdejgkcjamihkljkhonhcbableadmjfhofaomcfhgbifmendjhcgbgcejbnddijhbjjfmogjkamngcaodjknebfdnmdblikckcbicdej", baseStr="rcomcrewoankfivfgfcfkgeadtnmhagjczisqblyldidsuhfdsooviilaaosgswjbqzhkwfwdgqbnuypegvsqoborpnhtjjaxcpjwtxowutansksxzzydoottmknysmpxyetwhlghwbwqvsasmwjsdcgzgfudhettrasxayqdtjkykeyxvqzuvwtlfnoyvauiilqtfstoeilmzbncnxildnjaaoxezuflkthcgdjneycktavzmsovdtxpxjtcfxfkyvsfxnzrwdnnjcnkplctexlgqzipfvqcxfawzdznhbhtdkhfoirliqdfehsiviiimntnuzyfhnbgxakhozldclhxjzpgzyabxwrrfalqdfeaxtvkkucslhrypsjexjdhbeuuckolfgrvxecuhslcbezuxsoklmqpmobybqausuneushwgrrtafujsljzpprklapncxlwktpitejlujycahvvfvggrzayolskjvaauocfikptwiyntvqejfdxirsbmqqrilnmcopskbtdlozevmbiaxraspymfnyaxgazfxsuoygeqzoccmsxamtpcquxvrpsqzdqxaaouyjnrmqxlsvfwsfnmeqyvvufkvlzhzpsfqojrdntpxekahrknbrjtzmhrasbynxwnzfoishiaqmomkcgfsokrdmxmjyxrmrsaphcxicbfubzbvaeuevvvsidhgtzhxevtjxwiwriqkomltbaenbgxxdvzdcuddixkudbcoplqvyyzrkkkjxlqilowootcapiygayfpqzlhdfemmweynumqocfjlicesvbziglxvuqrimxbntrpaebvcbvdeqkepgnejlfkynpqzfamfylvogkakhgeuqqiegsvjoxjfgoiadlevwnwdwyljvvdzrzgiryhmepannhyfzvzxxbaxnqfrgnpksxomdojqvquwqagomfjubsrkxmnwdwfixbwmegdljzcjtdonufsoweesjlvjqivx"))
